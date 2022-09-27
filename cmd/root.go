659ceab27ea9e3410776ecf63f90a8e20093b602
Drew King <drew@drewcking.com>
2022-09-27 19:16:56 -0400

    version bump - v0.2.8

--- cmd/root.go
+++ cmd/root.go
@@ -14,7 +14,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "cw",
 	Short:   "Cyberwoven local web site development tool",
-	Version: "0.2.7",
+	Version: "0.2.8",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
