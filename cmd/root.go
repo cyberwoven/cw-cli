d3bf2c908bc63aaa0d7ccbd786fd0f94cabdffd2
Drew King <drew@drewcking.com>
2023-06-15 15:53:43 -0400

    v2.3.2 - fix the version

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:     "cw",
 	Short:   "Cyberwoven local web site development tool",
-	Version: "2.2.1",
+	Version: "2.3.2",
 }
 
 var ctx = cwutils.GetContext()
