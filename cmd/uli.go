de478f17f736aef76083629f92d6b70189fc33e6
Drew King <drew@drewcking.com>
2023-06-15 15:43:46 -0400

    v2.2.1 - fix uli after create, refactor task

--- cmd/uli.go
+++ cmd/uli.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"strconv"
 	"strings"
@@ -9,7 +10,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// uliCmd represents the uli command
 var uliCmd = &cobra.Command{
 	Use:   "uli",
 	Short: "Output `drush uli` with a link that works in the desired environment",
@@ -18,44 +18,25 @@ var uliCmd = &cobra.Command{
 		isFlaggedTest, _ := cmd.Flags().GetBool("test")
 		uid, _ := cmd.Flags().GetInt("uid")
 
-		var LOGIN_URL string
+		var url string
 		var stdout []byte
 		var err error
-		var drushCmd *exec.Cmd
 
 		if isFlaggedTest {
 			remoteUliCmd := fmt.Sprintf("cd %s && ~/bin/uli", ctx.DRUPAL_DEFAULT_DIR_REMOTE)
 			remoteHost := fmt.Sprintf("%s@%s", ctx.SSH_TEST_USER, ctx.SSH_TEST_HOST)
-			drushCmd = exec.Command("ssh", remoteHost, remoteUliCmd)
-		} else {
-			// local uli. let's nuke all sessions so we don't get the access denied error
-			err = exec.Command("drush", "sqlq", "TRUNCATE SESSIONS").Run()
+			drushCmd := exec.Command("ssh", remoteHost, remoteUliCmd)
+			stdout, err = drushCmd.Output()
 			if err != nil {
 				fmt.Println(err.Error())
+				return
 			}
-
-			drushCmd = exec.Command("drush", "uli", "--uid", strconv.Itoa(uid), "--uri", ctx.SITE_NAME+".test", "--no-browser")
+			url = strings.TrimSpace(string(stdout))
+		} else {
+			url = uliGenerateLink(ctx.PROJECT_ROOT, ctx.SITE_NAME+".test", uid)
 		}
 
-		stdout, err = drushCmd.Output()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		LOGIN_URL = strings.TrimSpace(string(stdout))
-		LOGIN_ARRAY := []string{LOGIN_URL, "?destination=admin/reports/status"}
-		LOGIN_URL_WITH_REDIRECT := strings.Join(LOGIN_ARRAY, "")
-
-		fmt.Println(LOGIN_URL_WITH_REDIRECT)
-
-		openCmd := exec.Command("open", LOGIN_URL_WITH_REDIRECT)
-
-		err = openCmd.Run()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-
+		uliOpenLink(url)
 	},
 }
 
@@ -63,4 +44,44 @@ func init() {
 	rootCmd.AddCommand(uliCmd)
 	uliCmd.Flags().Int("uid", 1, "Specific Drupal user ID to login as")
 	uliCmd.PersistentFlags().BoolP("test", "", false, "Run `drush uli` on the respective remote test environment.")
+}
+
+func uliOpenLink(url string) {
+	url += "?destination=admin/reports/status"
+
+	fmt.Println(url)
+
+	err := exec.Command("open", url).Run()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+func uliGenerateLink(drupalRoot string, uri string, uids ...int) string {
+
+	uid := 1
+	if len(uids) > 0 {
+		uid = uids[0]
+	}
+
+	var stdout []byte
+	var err error
+	var drushCmd *exec.Cmd
+
+	os.Chdir(drupalRoot)
+
+	// nuke all Drupal sessions so we don't get the access denied error
+	err = exec.Command("drush", "sqlq", "TRUNCATE SESSIONS").Run()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	drushCmd = exec.Command("drush", "uli", "--uid", strconv.Itoa(uid), "--uri", uri, "--no-browser")
+
+	stdout, err = drushCmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return strings.TrimSpace(string(stdout))
 }
