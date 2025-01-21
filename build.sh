30181de2b8844884d2ca9d129fc5dbb79fbdb507
Drew King <drew@drewcking.com>
2025-01-21 14:13:36 -0500

    v2.3.18 - only compile for ARM64 now

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 mkdir -p bin
-GOOS=darwin GOARCH=amd64 go build -trimpath -o bin/cw .
+
+# we compile specificaly for intel-compatibility, for older machines.
+# this means Apple Silicon machines will need to have Rosetta2 isntalled:
+#
+# Run this to see if you have an M1+ CPU to see if Rosetta is installed:
+#  arch -x86_64 zsh
+#
+# Run this to install it:
+#  softwareupdate --install-rosetta
+GOOS=darwin GOARCH=arm64 go build -trimpath -o bin/cw .
