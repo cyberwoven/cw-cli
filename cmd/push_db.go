3fa89a3f3ff7e394faa98de416568b70637cd8ec
Drew King <drew.king@cyberwoven.com>
2025-12-20 08:58:26 -0500

    v2.3.20 - cw push: use mysqldump instead of mydumper

--- cmd/push_db.go
+++ cmd/push_db.go
@@ -25,42 +25,54 @@ var pushDbCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		//
-		// Dump local DB
-		//
-		fmt.Println(" - Dump local database [schema]: " + ctx.DATABASE_NAME)
-		databaseDumpDir := ctx.DEFAULT_CONFIG_DIR + "/database_dumps/" + ctx.DATABASE_NAME
+		fmt.Println(" - Dump local database: " + ctx.DATABASE_NAME)
+		databaseDumpFile := fmt.Sprintf("%s/database_dumps/%s-push.sql.gz", ctx.DEFAULT_CONFIG_DIR, ctx.DATABASE_NAME)
 		err = exec.Command(
-			"mydumper",
-			"--no-data",
-			"--user", ctx.USERNAME,
-			"--database", ctx.DATABASE_NAME,
-			"--outputdir", databaseDumpDir,
-		).Run()
-		if err != nil {
-			fmt.Printf("Unable to dump schema for database %s\n", ctx.DATABASE_NAME)
-			log.Fatal(err)
-		}
-
-		fmt.Println(" - Dump local database [data]: " + ctx.DATABASE_NAME)
-		err = exec.Command(
-			"mydumper",
-			"--no-schemas",
-			"--user", ctx.USERNAME,
-			"--database", ctx.DATABASE_NAME,
-			"--regex", "^(?!(${DB}.watchdog|${DB}.cache|${DB}.session))",
-			"--outputdir", databaseDumpDir,
+			"sh",
+			"-c",
+			fmt.Sprintf("mysqldump %s | gzip > %s", ctx.DATABASE_NAME, databaseDumpFile),
 		).Run()
 		if err != nil {
 			fmt.Printf("Unable to dump data for database %s\n", ctx.DATABASE_NAME)
 			log.Fatal(err)
 		}
 
+		//
+		// Dump local DB
+		//
+		// fmt.Println(" - Dump local database [schema]: " + ctx.DATABASE_NAME)
+		// databaseDumpDir := ctx.DEFAULT_CONFIG_DIR + "/database_dumps/" + ctx.DATABASE_NAME
+		// err = exec.Command(
+		// 	"mydumper",
+		// 	"--no-data",
+		// 	"--user", ctx.USERNAME,
+		// 	"--database", ctx.DATABASE_NAME,
+		// 	"--outputdir", databaseDumpDir,
+		// ).Run()
+		// if err != nil {
+		// 	fmt.Printf("Unable to dump schema for database %s\n", ctx.DATABASE_NAME)
+		// 	log.Fatal(err)
+		// }
+
+		// fmt.Println(" - Dump local database [data]: " + ctx.DATABASE_NAME)
+		// err = exec.Command(
+		// 	"mydumper",
+		// 	"--no-schemas",
+		// 	"--user", ctx.USERNAME,
+		// 	"--database", ctx.DATABASE_NAME,
+		// 	"--regex", "^(?!(${DB}.watchdog|${DB}.cache|${DB}.session))",
+		// 	"--outputdir", databaseDumpDir,
+		// ).Run()
+		// if err != nil {
+		// 	fmt.Printf("Unable to dump data for database %s\n", ctx.DATABASE_NAME)
+		// 	log.Fatal(err)
+		// }
+
 		fmt.Println(" - rsync database files up to remote")
 		rsyncDest := fmt.Sprintf("%s@%s:~/backups/transient", ctx.SSH_TEST_USER, ctx.SSH_TEST_HOST)
-		err = exec.Command("rsync", "-avz", databaseDumpDir, rsyncDest, "--delete").Run()
+		err = exec.Command("rsync", databaseDumpFile, rsyncDest).Run()
 		if err != nil {
-			fmt.Printf("Unable to rsync database files up to remote host for database %s\n", ctx.DATABASE_NAME)
+			fmt.Printf("Unable to rsync database files up to remote host for database %s, local dump file: %s\n", ctx.DATABASE_NAME, databaseDumpFile)
 			log.Fatal(err)
 		}
 
@@ -69,6 +81,8 @@ var pushDbCmd = &cobra.Command{
 		//
 		fmt.Println(" - Load remote database: " + ctx.DATABASE_NAME)
 		remoteLoadCmd := fmt.Sprintf("bash ~/bin/database-load.sh %s", ctx.DATABASE_NAME)
+		// remoteBackupFile := fmt.Sprintf("~/backups/transient/%s-push.sql.gz", ctx.DATABASE_NAME)
+		// remoteLoadCmd := fmt.Sprintf("mysqladmin create %s; gunzip < %s | mysql %s", ctx.DATABASE_NAME, remoteBackupFile, ctx.DATABASE_NAME)
 		remoteHost := fmt.Sprintf("%s@%s", ctx.SSH_TEST_USER, ctx.SSH_TEST_HOST)
 		exec.Command("ssh", remoteHost, remoteLoadCmd).Run()
 
